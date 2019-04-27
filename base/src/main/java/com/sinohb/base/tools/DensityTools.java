f854acf7286dcabf69d0159aa8e786dc12d21a72
maikel0maikel <maikel@foxmail.com>
2019-04-27 20:29:31 +0800

    Update DensityTools.java
    sp转px

--- base/src/main/java/com/sinohb/base/tools/DensityTools.java
+++ base/src/main/java/com/sinohb/base/tools/DensityTools.java
@@ -20,12 +20,47 @@ public class DensityTools {
         return context.getResources().getDisplayMetrics().heightPixels;
     }
 
-    public static int dip2px(Context context, float dpValue) {
-        final float scale = context.getResources().getDisplayMetrics().density;
-        return (int) (dpValue * scale + 0.5f);
+     /**
+     * 将px值转换为dip或者dp值，保证尺寸大小不变
+     * @param context
+     * @param pxValue
+     * @return
+     */
+    public static int px2dip(Context context,float pxValue){
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int)(pxValue/scale+0.5f);
     }
 
-    public static int dp2px(Context context, float dp) {
-        return (int) (TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics()) + 0.5f);
+    /**
+     * 将dip或者dp值转换为px值，保证尺寸大小不变
+     * @param context
+     * @param dipValue
+     * @return
+     */
+    public static int dip2px(Context context,float dipValue){
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int)(dipValue*scale+0.5f);
+    }
+
+    /**
+     * 将px值转换为sp值，保证文字大小不变
+     * @param context
+     * @param pxValue
+     * @return
+     */
+    public static int px2sp(Context context,float pxValue){
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int)(pxValue/fontScale+0.5f);
+    }
+
+    /**
+     * 将sp值转为px值，保证文字大小不变
+     * @param context
+     * @param spValue
+     * @return
+     */
+    public static int sp2px(Context context,float spValue){
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int)(spValue*fontScale+0.5f);
     }
 }
